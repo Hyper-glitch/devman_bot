7d563604c3fceb80b878e28ac75e5940b6b977a1
Roman Smirnov <r.smirnov@okenit.ru>
2022-03-05 16:19:52 +0300

    feat:     - add condition to notification

--- tg_bot.py
+++ tg_bot.py
@@ -3,6 +3,15 @@ import os
 import telegram
 
 
-def send_notification(your_name):
+def send_notification(your_name, lesson_title, is_negative_result, lesson_url):
     bot = telegram.Bot(token=os.environ.get('TG_TOKEN'))
-    bot.send_message(chat_id=os.environ.get('CHAT_ID'), text=f"{your_name}, the teacher checked the work!")
+
+    if is_negative_result:
+        result_text = "Unfortunately your work has mistakes"
+    else:
+        result_text = "Good job, keep going br!"
+
+    bot.send_message(chat_id=os.environ.get('CHAT_ID'),
+                     text=f"{your_name}, the teacher checked the work <<{lesson_title}>>" +
+                          f"\nHere is an url for the lesson: {lesson_url}\n" + result_text
+                     )
