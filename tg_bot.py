3bb967744628517684b294fa7d2f5ea74bc4d450
Roman <graff049@gmail.com>
2022-03-08 20:09:16 +0300

    1. name refactor

--- tg_bot.py
+++ tg_bot.py
@@ -3,15 +3,15 @@ import os
 import telegram
 
 
-def send_notification(your_name, lesson_title, is_negative_result, lesson_url):
+def send_notification(username, lesson_title, is_lesson_failed, lesson_url):
     bot = telegram.Bot(token=os.environ.get('TG_TOKEN'))
 
-    if is_negative_result:
-        result_text = "Unfortunately your work has mistakes"
+    if is_lesson_failed:
+        text = "Unfortunately your work has mistakes"
     else:
-        result_text = "Good job, keep going br!"
+        text = "Good job, keep going br!"
 
     bot.send_message(chat_id=os.environ.get('CHAT_ID'),
-                     text=f"{your_name}, the teacher checked the work <<{lesson_title}>>" +
-                          f"\nHere is an url for the lesson: {lesson_url}\n" + result_text
+                     text=f"{username}, the teacher checked the work <<{lesson_title}>>" +
+                          f"\nHere is an url for the lesson: {lesson_url}\n" + text
                      )
