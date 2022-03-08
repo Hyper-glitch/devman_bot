949604a37f9452bc9d7eb00b6e62379175116551
Roman <graff049@gmail.com>
2022-03-08 21:01:13 +0300

    1. add bot instance to send notification

--- tg_bot.py
+++ tg_bot.py
@@ -1,17 +1,13 @@
 import os
 
-import telegram
 
-
-def send_notification(username, lesson_title, is_lesson_failed, lesson_url):
-    bot = telegram.Bot(token=os.environ.get('TG_TOKEN'))
-
+def send_notification(username, lesson_title, is_lesson_failed, lesson_url, telegram_bot):
     if is_lesson_failed:
         text = "Unfortunately your work has mistakes"
     else:
         text = "Good job, keep going br!"
 
-    bot.send_message(chat_id=os.environ.get('CHAT_ID'),
-                     text=f"{username}, the teacher checked the work <<{lesson_title}>>" +
-                          f"\nHere is an url for the lesson: {lesson_url}\n" + text
-                     )
+    telegram_bot.send_message(chat_id=os.environ.get('CHAT_ID'),
+                              text=f"{username}, the teacher checked the work <<{lesson_title}>>" +
+                                   f"\nHere is an url for the lesson: {lesson_url}\n" + text
+                              )
