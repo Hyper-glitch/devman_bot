6af774c642a6d5f3cf5f02b6f0ea124d7a50ce2e
Roman <graff049@gmail.com>
2022-03-08 21:50:51 +0300

    1. collect all env vars in main

--- tg_bot.py
+++ tg_bot.py
@@ -1,13 +1,10 @@
-import os
-
-
-def send_notification(username, lesson_title, is_lesson_failed, lesson_url, telegram_bot):
+def send_notification(username, lesson_title, is_lesson_failed, lesson_url, telegram_bot, chat_id):
     if is_lesson_failed:
         text = "Unfortunately your work has mistakes"
     else:
         text = "Good job, keep going br!"
 
-    telegram_bot.send_message(chat_id=os.environ.get('CHAT_ID'),
+    telegram_bot.send_message(chat_id=chat_id,
                               text=f"{username}, the teacher checked the work <<{lesson_title}>>" +
                                    f"\nHere is an url for the lesson: {lesson_url}\n" + text
                               )
