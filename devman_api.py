100088f635ba21692df315ad2c758491331f3dc8
Roman Smirnov <r.smirnov@okenit.ru>
2022-03-10 17:19:17 +0300

    test

--- devman_api.py
+++ devman_api.py
@@ -71,3 +71,4 @@ if __name__ == '__main__':
     user_reviews = api.get_user_reviews()
     print(user_reviews)
     api.get_long_polling(telegram_bot=telegram_bot, username=username, chat_id=chat_id)
+
