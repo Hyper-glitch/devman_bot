ffe20c2dab0682aef872225c037654d758a17d98
Roman <graff049@gmail.com>
2022-04-18 21:50:50 +0300

    1. move main script logic to a new file

---
+++ notification_bot.py
@@ -0,0 +1,33 @@
+import logging
+
+import telegram
+from dotenv import load_dotenv
+
+from bot_settings import DEVMAN_TOKEN, TELEGRAM_TOKEN, USERNAME, CHAT_ID
+from devman_api import ApiDevMan
+from utils import TelegramLogsHandler
+
+
+def main():
+    logger.info('Bot is running')
+    load_dotenv()
+    logger_format = '%(asctime)s %(filename)s %(levelname)s %(message)s'
+
+    telegram_bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    set_up_logger(telegram_bot=telegram_bot, chat_id=CHAT_ID, logger_format=logger_format)
+    api = ApiDevMan(devman_token=DEVMAN_TOKEN)
+
+    api.get_long_polling(telegram_bot=telegram_bot, username=USERNAME, chat_id=CHAT_ID)
+
+
+def set_up_logger(telegram_bot, chat_id, logger_format):
+    logging.basicConfig(level=logging.INFO, format=logger_format)
+
+    tg_handler = TelegramLogsHandler(telegram_bot, chat_id)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(tg_handler)
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger('devman_bot')
+    main()
