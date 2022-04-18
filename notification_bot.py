3b19077430bc9450abca20e80bea900b83fd664c
Roman <graff049@gmail.com>
2022-04-18 22:40:44 +0300

    1. make some fun refactoring

--- notification_bot.py
+++ notification_bot.py
@@ -4,28 +4,29 @@ import telegram
 
 from bot_settings import DEVMAN_TOKEN, TELEGRAM_TOKEN, USERNAME, CHAT_ID
 from devman_api import ApiDevMan
-from utils import TelegramLogsHandler
+from tg_bot import TelegramLogsHandler
+
+logger = logging.getLogger('devman_bot')
 
 
-def main():
+def main(telegram_bot):
     logger.info('Bot is running')
-    logger_format = '%(asctime)s %(filename)s %(levelname)s %(message)s'
-
-    telegram_bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    set_up_logger(telegram_bot=telegram_bot, chat_id=CHAT_ID, logger_format=logger_format)
     api = ApiDevMan(devman_token=DEVMAN_TOKEN)
 
     api.get_long_polling(telegram_bot=telegram_bot, username=USERNAME, chat_id=CHAT_ID)
 
 
-def set_up_logger(telegram_bot, chat_id, logger_format):
+def set_up_logger(telegram_bot, chat_id):
+    logger_format = '%(asctime)s %(filename)s %(levelname)s %(message)s'
     logging.basicConfig(level=logging.INFO, format=logger_format)
 
     tg_handler = TelegramLogsHandler(telegram_bot, chat_id)
+
     logger.setLevel(logging.INFO)
     logger.addHandler(tg_handler)
 
 
 if __name__ == '__main__':
-    logger = logging.getLogger('devman_bot')
-    main()
+    telegram_bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    set_up_logger(telegram_bot=telegram_bot, chat_id=CHAT_ID)
+    main(telegram_bot=telegram_bot)
