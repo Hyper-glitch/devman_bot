3e1f6f444fef18df57ed32f1e9679a07259704f5
Roman <graff049@gmail.com>
2022-04-19 23:14:56 +0300

    1. rename vars

--- bot/main.py
+++ bot/main.py
@@ -17,13 +17,13 @@ def main():
     load_dotenv()
     devman_token = os.getenv('DEVMAN_TOKEN')
     telegram_token = os.getenv('TG_TOKEN')
-    username = os.getenv('USERNAME')
-    chat_id = os.getenv('CHAT_ID')
+    username = os.getenv('TG_USERNAME')
+    chat_id = os.getenv('TG_CHAT_ID')
 
     api = ApiDevMan(devman_token=devman_token)
     telegram_bot = telegram.Bot(token=telegram_token)
-    tg_handler = TelegramLogsHandler(telegram_bot, chat_id)
-    logger.addHandler(tg_handler)
+    telegram_handler = TelegramLogsHandler(telegram_bot, chat_id)
+    logger.addHandler(telegram_handler)
 
     api.get_long_polling(telegram_bot=telegram_bot, username=username, chat_id=chat_id)
 
